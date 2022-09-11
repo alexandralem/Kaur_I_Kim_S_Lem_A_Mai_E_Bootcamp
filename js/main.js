2807b0e8beebaee662e3003224fadfea4a7a862c
Alexandra Lem <a_lem@fanshaweonline.ca>
2022-09-11 17:23:04 -0400

    adding js

--- js/main.js
+++ js/main.js
@@ -4,4 +4,38 @@ const navUl = document.getElementById('nav-ul');
 hamburger.addEventListener('click', () => {
     navUl.classList.toggle('show');
 })
+
+const   icon = document.querySelectorAll('.iconContainer'),
+        thePhotos = document.getElementById('photos'),
+        memberName = document.querySelector('#name'),
+        bioInfo = document.querySelector('.bio-info');
+
+let currentMember = '';
+const bioText = [
+    `Heyya! My name is Ishpreet. I work as a Graphic Designer in this company. I love to design modern and engaging logos, business cards, illustrations, layouts, magazines, product packaging and more. I put all my efforts to make the clients satisfied with my creativity.`,
+
+    `tttttttttt`,
+
+    `hhhhhhhhh`,
+
+    `rrrrrrrrrr.`
+]
+
+function animatePhotos() {
+    
+    thePhotos.style.right = `${this.dataset.offset * 300}px`;
+    getBioText(this.classList, this.dataset.offset);
+
+    currentMember = this.classList[1];
+  }
+
+  function getBioText(name, index) {
+    console.log(name[1]);
+    memberName.textContent = `${name[1]}`;
+    bioInfo.textContent = bioText[index];
+  }
+
+
   
+
+  icon.forEach(icon => icon.addEventListener('click', animatePhotos));
