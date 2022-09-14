9e8ef4588d3fb1976aad7b8e3adba2afa02e0a6a
Alexandra Lem <a_lem@fanshaweonline.ca>
2022-09-13 23:22:40 -0400

    fixing a conflict

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+(() => {
 const hamburger = document.getElementById('hamburger');
 const navUl = document.getElementById('nav-ul');
 
@@ -14,11 +15,11 @@ let currentMember = '';
 const bioText = [
     `Heyya! My name is Ishpreet. I work as a Graphic Designer in this company. I love to design modern and engaging logos, business cards, illustrations, layouts, magazines, product packaging and more. I put all my efforts to make the clients satisfied with my creativity.`,
 
-    `tttttttttt`,
+    ``,
 
     `Hi! I'm Alex. And I am a developer of this team. I like front-end developing because I enjoy watching something new being born from a few lines of code. I find crushing bugs in code to be somewhat similar to solving riddles. And what can be better than a good riddle`,
 
-    `rrrrrrrrrr.`
+    `Hi, Evica strives to become a become a versatile designer capable of various styles and art mediums. Whether it is creating website, indie games, prints, animations and many other forms of digital media, she loves creating a product exuding her style while staying authentic to the message and goal of the task. In fact, she is passionate on projects that strikes the perfect balance between user experience and design. She is ready to take on anything with her pink pastel attitude.`
 ]
 
 function animatePhotos() {
@@ -31,7 +32,7 @@ function animatePhotos() {
 
   function getBioText(name, index) {
     console.log(name[1]);
-    memberName.textContent = `${name[1]}`;
+    memberName.textContent = `${name[1].charAt(0).toUpperCase() + name[1].slice(1)}`;
     bioInfo.textContent = bioText[index];
   }
 
@@ -39,3 +40,7 @@ function animatePhotos() {
   
 
   icon.forEach(icon => icon.addEventListener('click', animatePhotos));
+
+  
+
+})();
